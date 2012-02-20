17b1023395e1bad7fd970c81b3d80d1ee58de5aa
John Nairn <jnairn@nairnmac.forestry.oregonstate.edu>
2012-02-20 15:51:25 -0800

    Get main screen visible rect and .gitignore files

--- trunk/Scripts/Development/Miscellaneous/Decode Lat Lon.py
+++ trunk/Scripts/Development/Miscellaneous/Decode Lat Lon.py
@@ -6,12 +6,22 @@
 from GEDitCOMII import *
 import math
 
+# Get size of main screen only (one with menu bar
+# Return visible area [x,y,width,height] accounting for dock and menu bar
+def GetMainScreenSize() :
+    screen = NSScreen.mainScreen()
+    screenRect = screen.visibleFrame()
+    return [screenRect.origin.x,screenRect.origin.y,NSWidth(screenRect),NSHeight(screenRect)]
+    
 ################### Main Script
 
 # Preamble
 gedit = CheckVersionAndDocument("Decode Lat Lon",1.6,2)
 if not(gedit) : quit()
 gdoc = FrontDocument()
+
+print str(GetScreenSize())
+print str(GetMainScreenSize())
 
 print str(kmPerLongitudeDegree(0.))
 trials = ["-75.1697","45","-1"]
